c4f21e22f84daf0f2262bb35261e5bb08f57fda9
Fryer <Fryer@users.noreply.github.com>
2023-12-05 20:28:23 +0100

    Improve prompting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use std::io::Write;
+use std::time::Instant;
 use crate::days::Part;
 
 mod days;
@@ -11,58 +12,88 @@ mod day05;
 
 const DAYS: &[days::Day] = &[
     &[
-        Part::new("Part 1", day01::part1),
-        Part::new("Part 2", day01::part2),
+        Part::new("Day 1, part 1", day01::part1),
+        Part::new("Day 1, part 2", day01::part2),
     ],
     &[
-        Part::new("Part 1", day02::part1),
-        Part::new("Part 2", day02::part2),
+        Part::new("Day 2, part 1", day02::part1),
+        Part::new("Day 2, part 2", day02::part2),
     ],
     &[
-        Part::new("Part 1", day03::part1),
-        Part::new("Part 2", day03::part2),
+        Part::new("Day 3, part 1", day03::part1),
+        Part::new("Day 3, part 2", day03::part2),
     ],
     &[
-        Part::new("Part 1", day04::part1),
-        Part::new("Part 2", day04::part2),
+        Part::new("Day 4, part 1", day04::part1),
+        Part::new("Day 4, part 2", day04::part2),
     ],
     &[
-        Part::new("Part 1", day05::part1),
-        Part::new("Part 2", day05::part2),
+        Part::new("Day 5, part 1", day05::part1),
+        Part::new("Day 5, part 2", day05::part2),
     ],
 ];
 
 fn main() {
-    print!("Day: ");
+    print!("Day (default = all): ");
     io::stdout().flush().unwrap();
-    let mut io_input = String::new();
-    io::stdin().read_line(&mut io_input).unwrap();
-    let day: usize = match io_input.trim_end().parse() {
+    let day_input = io::stdin().lines().next().unwrap().unwrap();
+    let timer = Instant::now();
+    if day_input.is_empty() {
+        for day in 1..=DAYS.len() {
+            match read_day_input(day) {
+                Ok(input) => run_day(day, &input),
+                Err(_) => return,
+            };
+        }
+    }
+    else {
+        let day: usize = match parse_day(&day_input) {
+            Ok(day) => day,
+            Err(_) => return,
+        };
+        match read_day_input(day) {
+            Ok(input) => run_day(day, &input),
+            Err(_) => return,
+        };
+    }
+    println!("=== Done! ===");
+    println!("Total time: {} µs", timer.elapsed().as_micros());
+}
+
+fn parse_day(text: &str) -> Result<usize, ()> {
+    let day: usize = match text.trim_end().parse() {
         Ok(day) => day,
         Err(_) => {
             println!("Day must be 1-25.");
-            return;
-        }
+            return Err(());
+        },
     };
     if day < 1 || day > 25 {
         println!("Day must be 1-25.");
-        return;
+        return Err(());
     }
     if day > DAYS.len() {
         println!("Day {} not implemented.", day);
-        return;
+        return Err(());
     }
-    let input = match fs::read_to_string(format!("input/day{:02}.txt", day)) {
-        Ok(data) => data.replace('\r', ""),
+    return Ok(day);
+}
+
+fn read_day_input(day: usize) -> Result<String, ()> {
+    return match fs::read_to_string(format!("input/day{:02}.txt", day)) {
+        Ok(data) => Ok(data.replace('\r', "")),
         Err(_) => {
             println!("Input file \"input/day{:02}.txt\" not found.", day);
-            return;
-        }
+            return Err(());
+        },
     };
+}
+
+fn run_day(day: usize, input: &str) {
     for part in DAYS[day - 1] {
         println!("=== {} ===", part.name);
-        let start = std::time::Instant::now();
-        (part.run)(&input);
-        println!("Time: {} µs", start.elapsed().as_micros());
+        let timer = Instant::now();
+        (part.run)(input);
+        println!("Time: {} µs", timer.elapsed().as_micros());
     }
 }
